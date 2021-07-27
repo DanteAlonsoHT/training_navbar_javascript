a912f00ad3c891761f19408ee1a23a56d374d199
DanteAlonsoHT <dante1818@hotmail.com>
2021-07-26 22:33:39 -0500

    Fix linters for javascripts

--- js/script.js
+++ js/script.js
@@ -1,44 +1,35 @@
-window.onload = function(){
+window.onload = function () {
+  const menuLogo = document.querySelector('#menu-bar');
+  const menuContent = document.querySelector('#mobile-nav');
+  const closeIcon = document.querySelector('.close_icon');
+  const logo = document.querySelector('#logo');
+  const header = document.querySelector('header');
 
-    const menu_logo = document.querySelector('#menu-bar');
-    const menu_content = document.querySelector('#mobile-nav');
-    const close_icon = document.querySelector(".close_icon")
-    const logo = document.querySelector('#logo');
-    const header = document.querySelector('header');
-    const link = document.querySelector(".nav-link")
+  function openMenu() {
+    menuContent.classList.toggle('show');
+    logo.classList.toggle('no_show');
+    menuLogo.classList.toggle('no_show');
+    header.classList.toggle('header-padding');
+  }
 
-    menu_logo.onclick = function() {openMenu()};
+  menuLogo.onclick = function () { openMenu(); };
 
-    function openMenu() {
-        menu_content.classList.toggle("show")
-        logo.classList.toggle("no_show")
-        menu_logo.classList.toggle("no_show")
-        header.classList.toggle("header-padding")
-    };
+  function closeMenu() {
+    menuContent.classList.toggle('show');
+    logo.classList.toggle('no_show');
+    header.classList.toggle('header-padding');
+    menuLogo.classList.toggle('no_show');
+  }
 
-    close_icon.onclick = function() {closeMenu()};
+  closeIcon.onclick = function () { closeMenu(); };
 
-    function closeMenu() {
-        menu_content.classList.toggle("show")
-        logo.classList.toggle("no_show")
-        header.classList.toggle("header-padding")
-        menu_logo.classList.toggle("no_show")
-    };
-
-    menu_content.addEventListener('click', e=>{
-
-        console.log(e.target)
-        console.log(e.target == link)
-        console.log(link)
-        console.log(e.target == menu_content)
-        console.log(menu_content)
-
-        if (menu_content.classList.contains("show") && e.target != menu_content 
-            && e.target != menu_logo){
-            menu_content.classList.toggle("show")
-            logo.classList.toggle("no_show")
-            menu_logo.classList.toggle("no_show")
-            header.classList.toggle("header-padding")
-        }
-    });
+  menuContent.addEventListener('click', (e) => {
+    if (menuContent.classList.contains('show') && e.target !== menuContent
+            && e.target !== menuLogo) {
+      menuContent.classList.toggle('show');
+      logo.classList.toggle('no_show');
+      menuLogo.classList.toggle('no_show');
+      header.classList.toggle('header-padding');
+    }
+  });
 };
